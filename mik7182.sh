e3e07e2a48bd1701386bbb67e30c2f383b7b9457
gejne777 <155109726+gejne777@users.noreply.github.com>
2025-05-26 20:48:58 +0300

    Update mik7192.sh

--- mik7182.sh
+++ mik7182.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 sleep 3
-wget https://github.com/elseif/MikroTikPatch/releases/download/7.18.2/chr-7.18.2.img.zip -O chr.img.zip  && \
+wget https://github.com/elseif/MikroTikPatch/releases/download/7.19/chr-7.19.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 echo STORAGE is $STORAGE && \
